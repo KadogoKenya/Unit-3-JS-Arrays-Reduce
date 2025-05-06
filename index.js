320740e60bbe504ade5f70de6315090785b16e38
KadogoKenya <catherinekanini001@gmail.com>
2025-05-06 23:27:46 +0300

    array reduce method

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (total, batch) {
+  return total + batch;
+}, 0);
+
+// OR using arrow function:
+const totalBatteriesNew = batteryBatches.reduce((total, batch) => total + batch, 0);
+
